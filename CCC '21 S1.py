4933d72565462ee582e008143f4f311769271fc2
Arnav Dalmia <70185864+ArnavDalmia@users.noreply.github.com>
2021-11-28 14:52:37 -0500

    Add files via upload

---
+++ CCC '21 S1.py
@@ -0,0 +1,12 @@
+counter = int(input())
+sides = list(map(int, input().split()))
+bases = list(map(int, input().split()))
+
+
+total = 0 
+for i in range(counter):
+    total += ((sides[i] + sides[i+1]) * bases[i]) / 2
+
+
+print(total)
+
