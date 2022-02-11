a208cbdd8028b9234ee96c21a0739f7191015845
Arnav Dalmia <70185864+ArnavDalmia@users.noreply.github.com>
2022-02-10 20:56:34 -0500

    Add files via upload

---
+++ J3_21.py
@@ -0,0 +1,16 @@
+prev = ""
+
+while True:
+    sq = input()
+    dir = ""
+    if sq == "99999":
+        break
+    p1 = int(sq[1])+int(sq[0])
+    if p1 == 0:
+        dir = prev
+    elif p1 % 2 == 0:
+        dir = "right"
+    elif p1 % 2 == 1:
+        dir = "left"
+    print(dir + " " + sq[2:])
+    prev = dir
